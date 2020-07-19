8285a934fa5c705f32c61940879c8322470060cf
Patrick Garcia <garciahernando.patrick@gmail.com>
2020-07-19 18:14:36 +0200

    changes

--- src/main/java/com/mvc/App/entity/Patient.java
+++ src/main/java/com/mvc/App/entity/Patient.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -19,9 +20,13 @@ public class Patient {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private long id;
+
     private String firstName;
+
     private String lastName;
+
     private String email;
+
     private Tested test;
 
 }
