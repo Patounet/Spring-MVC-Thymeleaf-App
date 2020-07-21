07cf912db42637f633dcc0c8dbe30e35da7a0c1c
Patrick Garcia <garciahernando.patrick@gmail.com>
2020-07-21 12:34:01 +0200

    changes

--- src/main/java/com/mvc/App/controller/PatientController.java
+++ src/main/java/com/mvc/App/controller/PatientController.java
@@ -1,6 +1,6 @@
 package com.mvc.App.controller;
 import com.mvc.App.entity.Patient;
-import com.mvc.App.service.PatientServiceimpl;
+import com.mvc.App.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 public class PatientController {
 
         @Autowired
-        PatientServiceimpl patientService;
+        PatientService patientService;
 
         @GetMapping("/")
         public String patientsList(Model model) {
